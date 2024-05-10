7fb8024662c74fb8138f64f58bf4cd8efb611622
Jonas Scholz <scholzjonas99@gmail.com>
2024-05-10 17:47:16 +0200

    add delta Rotation

--- main.js
+++ main.js
@@ -264,9 +264,9 @@ function onXRFrame(t, frame) {
     three_renderer.render( three_scene, three_camera );
     if(splat_placed) {
         let deltaPosition = three_camera.position.clone().sub(three_camera_setup_position);
-        console.log("delta Position: (" + (three_camera.position.x-three_camera_setup_position.x) + ", " + (three_camera.position.y-three_camera_setup_position.y) + ", " + (three_camera.position.z-three_camera_setup_position.z) + ")")
-        // let deltaRotation =  three_camera.quaternion.multiply(three_camera_setup_rotation.invert());
-        
+        console.log("delta Position: (" + deltaPosition.x + ", " + deltaPosition.y + ", " + deltaPosition.z + ")")
+        let deltaRotation = three_camera.quaternion.clone().multiply(three_camera_setup_quaternion.clone().invert());
+        console.log("delta Rotation: (" + deltaRotation.x + ", " + deltaRotation.y + ", " + deltaRotation.z + ", " + deltaRotation.w + ")")
         // splat_camera._position.x = scale*movement_scale*three_camera.position.x;
         // splat_camera._position.y = -scale*movement_scale*three_camera.position.y-initial_y;
         // splat_camera._position.z = -scale*movement_scale*three_camera.position.z-initial_z;
